3620d01c9cff9f4120c7780c973309427f45544f
Marco <5439724524@student.cms.k12.nc.us>
2026-01-22 08:27:21 -0500

    Update loadapps.js

--- resources/loadapps.js
+++ resources/loadapps.js
@@ -6,6 +6,7 @@ const classlink = document.getElementById('classlink');
 const chesskid = document.getElementById('chesskid');
 const cli = document.getElementById('cli');
 const cki = document.getElementById('cki');
+const cai = document.getElementById('cai');
 
 async function getJSON() {
   res = await fetch(`links.json`);
@@ -17,6 +18,7 @@ classlink.href = link.classlink;
 chesskid.href = link.chesskid;
 cli.src = link.cli;
 cki.src = link.cki;
+cai.src = link.cai;
   
   });
 });
